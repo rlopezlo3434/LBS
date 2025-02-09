54e7ed056bf7ac267b4433be5c31d99ca10f9d85
Renzo <152234907+rlopezlo3434@users.noreply.github.com>
2025-02-09 07:55:48 -0500

    second commit

--- src/app/pages/pagina4/pagina4.component.ts
+++ src/app/pages/pagina4/pagina4.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormularioService } from 'src/app/services/services/formulario.service';
 
 @Component({
   selector: 'app-pagina4',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./pagina4.component.css']
 })
 export class Pagina4Component {
+  datos: any;
 
+  constructor(private formularioService: FormularioService, private router: Router) {
+      this.datos = this.formularioService.obtenerDatos();
+      console.log(this.formularioService.obtenerJSON());
+      // this.jsonData = this.formularioService.obtenerJSON();
+  
+      // this.opcionesFormateadas = this.jsonData.map(item => {
+      //   let codigoStr = item.CODIGO.toString().padStart(9, '0');
+      //   return `${item.CCPP} - ${codigoStr}`;
+      // });
+  
+      this.formularioService.actualizarDatos(this.datos);
+    }
 }
